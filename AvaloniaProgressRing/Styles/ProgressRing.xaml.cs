1761fe157dcf3e602a0e152fc9d5ef60cf80aff2
Fernando Lucas <info@fernandolucas.info>
2025-12-09 23:23:44 +0100

    update

---
+++ AvaloniaProgressRing/Styles/ProgressRing.xaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaProgressRing.Styles
+{
+    public partial class ProgressRing : Avalonia.Styling.Styles
+    {
+        public ProgressRing()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
